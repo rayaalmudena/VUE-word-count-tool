e27b0927857da13c5c69ab81499140cb98a00c2f
rayaalmudena <raya.almudena@gmail.com>
2022-04-06 22:02:39 +0200

    done

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-let button = document.getElementById('btn');
-
-button.addEventListener('click', function(){
-    let word = document.getElementById('str').value;
-    let count = word.length;
-    let outputDiv = document.getElementById('output');
-
-    outputDiv.innerHTML = `<h1>${count}</h1>`
-});
+Vue.createApp({
+    data() {
+      return {
+        word: '',
+        wordLenght:'',
+        show:false
+      }
+    },
+    methods:{
+        calculateWordLenght(){
+            this.wordLenght=this.word.length;
+            this.show=true;
+        }
+    }
+  }).mount('#app')
